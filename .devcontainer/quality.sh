187a1953868f0bfedb558ac0c0f41f2741760ebf
WeAreRaraAvis <103233544+WeAreRaraAvis@users.noreply.github.com>
2023-12-13 02:58:27 +0000

    Modify Cypress Access Without Login from Within Codespace

--- .devcontainer/quality.sh
+++ .devcontainer/quality.sh
@@ -14,7 +14,9 @@ CYPRESS_CACHE_DIR="/workspaces/Tecumseh-Boilerplate/.cypress-caching"
 # Check if the Cypress cache directory exists
 if [ -d "$CYPRESS_CACHE_DIR" ]; then
     echo "Cypress cache found. Opening Cypress..."
+    echo "The Cypress GUI: https://${CODESPACE_NAME}-6080.app.github.dev/?autoconnect=true&password=vscode"
     npx cypress open
+    
 else
     echo "Cypress cache not found. Installing Cypress..."
     npx cypress install
