26a8acfd1b7c22c12385ef1e66809966196f6123
WeAreRaraAvis <103233544+WeAreRaraAvis@users.noreply.github.com>
2024-02-09 01:17:24 +0000

    I like reruns.

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    // Function to handle events in the Node.js environment
+    setupNodeEvents(on, config) {
+      // Node event listeners can be specified here
+    },
+    // Specify the pattern to match your single test file
+    specPattern: 'cypress/firstTest.cy.js',
+    // Since no baseUrl is preferred, it is omitted
+    chromeWebSecurity: false,
+    video: false, // Video recording is disabled for faster test execution
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000
+  }
+});
